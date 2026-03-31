3f392156fba8ee97857614c7a89e779c924928be
salah sayed <88517685+salah3bkrino@users.noreply.github.com>
2026-03-31 08:57:35 +0200

    style: white-label Chatwoot as Automation Service with purple theme

--- app/javascript/shared/store/globalConfig.js
+++ app/javascript/shared/store/globalConfig.js
@@ -54,8 +54,8 @@ const state = {
 export const getters = {
   get: $state => $state,
   isOnChatwootCloud: $state => $state.deploymentEnv === 'cloud',
-  isACustomBrandedInstance: $state => $state.installationName !== 'Chatwoot',
-  isAChatwootInstance: $state => $state.installationName === 'Chatwoot',
+  isACustomBrandedInstance: $state => $state.installationName !== 'Automation Service',
+  isAChatwootInstance: $state => $state.installationName === 'Automation Service',
 };
 
 export const actions = {};
